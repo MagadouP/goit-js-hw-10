1519721da8888db95ace04ed843a9b18c00c01c0
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-24 18:49:20 +0000

    Deploying to gh-pages from @ MagadouP/goit-js-hw-10@fd967d7370ee50e2f7afe5a0f649f107042c8260 🚀

--- index.27ed3f7e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box"),document.querySelector("#country-list"),document.querySelector("#country-info"),document.querySelector(".container");
-//# sourceMappingURL=index.27ed3f7e.js.map
--- index.642ff5f1.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box"),document.querySelector("#country-list"),document.querySelector("#country-info"),document.querySelector(".container");
-//# sourceMappingURL=index.642ff5f1.js.map
