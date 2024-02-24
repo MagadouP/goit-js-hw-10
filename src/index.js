b4d6082a924a3e67edd63eae375b841f7fa5c3f1
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-24 18:10:05 +0100

    Update index.js
    const

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import {fetchCountries} from './fetchCountries';
 import debounce from 'loadash.debounce';
 import Notiflix from 'notiflix';
 
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('#country-list');
+const countryInfo = document.querySelector('#country-info');
+const container = document.querySelector('.container');
+
+
