76cac8624fcdc7919ce39b7ca35d878c736dd3c9
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-25 15:48:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import Notiflix from 'notiflix';
 
 
 const searchBox = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('#country-list');
+const countryInfo = document.querySelector('#country-info');
 const container = document.querySelector('.container');
 
 function clearContainer() {
