7ad1d9cea2a1fac654bf53b60c0bc842aaa3e23a
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-25 15:18:34 +0100

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,57 @@
-const URL = 'https://restcountries.com/v3.1/name/';
-const FIELDS = 'fields=name,capital,population,flags,languages';
+import Notiflix from 'notiflix';
 
-export async function fetchCountries(name) {
-    const response = await fetch(`${URL}${name}?${FIELDS}`);
-if (response.ok) {
-    return await response.json();
-} else {
-    throw new Error ('Country not found');
-}
-}
+const countryList = document.querySelector('ul.country-list');
+
+const getUrl = name =>
+  `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+
+const displayCountries = ({ flags, name, capital, population, languages }) => {
+  const country = document.createElement('div');
+  const countryLanguages = languages.map(language => language.name).join(', ');
+
+  country.innerHTML = `<h1><img src="${flags.svg}" alt="${name} flag" width="24px"/>
+  ${name} </h1>
+  <div><b>Capital:</b> ${capital}</div>
+  <div><b>Population:</b> ${population}</div>
+  <div><b>Languages:</b> ${countryLanguages}</div>`;
+
+  countryList.append(country);
+};
+
+const displayCountriesList = countries => {
+  const listOfCountries = countries.map(({ flags, name }) => {
+    const list = document.createElement('div');
+
+    list.innerHTML = `<h1><img src="${flags.svg}" alt="${name} flag" width="24px"/>
+    ${name} </h1>`;
+
+    return list;
+  });
+  countryList.append(...listOfCountries);
+};
+
+const fetchCountries = name => {
+  const trimName = name.trim();
+  if (trimName.length === 0) return;
+  return fetch(getUrl(trimName))
+    .then(response => response.json())
+    .then(countries => {
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (countries.status === 404) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        return;
+      }
+      if ((countries.length <= 10) & (countries.length >= 2)) {
+        return displayCountriesList(countries);
+      }
+      if (countries.length === 1) {
+        return displayCountries(countries[0]);
+      }
+    });
+};
+
+export default fetchCountries;
