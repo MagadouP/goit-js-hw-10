5ad155255736c3fab5b263e1bcc2b70e88100a67
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-25 09:51:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { fetchCountries } from "./fetchCountries";
 import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
+
 
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('#country-list');
